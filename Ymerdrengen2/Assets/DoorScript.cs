ca28aa58f25da45792de00fc25abd1cc65fbd74c
Angeliki Skandalou <agg.skandalou@gmail.com>
2016-09-28 15:26:54 +0200

    max and num of ymer player can hold

---
+++ Ymerdrengen2/Assets/DoorScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void ChangeLevel()
+    {
+        //getTile(currentTile).IsDoor();
+    }
+}
