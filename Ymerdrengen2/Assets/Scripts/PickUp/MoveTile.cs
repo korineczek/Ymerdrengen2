b0127a15e0bf00c0e5793b84feb57db1131783a2
Angeliki Skandalou <agg.skandalou@gmail.com>
2016-09-28 09:06:11 +0200

    pickup stuff

---
+++ Ymerdrengen2/Assets/Scripts/PickUp/MoveTile.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class MoveTile : MonoBehaviour
+{
+    public void Move(MoveDirection dir, int NumberofTiles)
+    {
+        switch (dir)
+        {
+            case MoveDirection.LeftUp:
+                {
+                    transform.Translate(0, 0, NumberofTiles);
+                    break;
+                }
+            case MoveDirection.RightUp:
+                {
+                    transform.Translate(NumberofTiles, 0, 0);
+                    break;
+                }
+            case MoveDirection.RightDown:
+                {
+                    transform.Translate(0, 0, -NumberofTiles);
+                    break;
+                }
+            case MoveDirection.LeftDown:
+                {
+                    transform.Translate(-NumberofTiles, 0, 0);
+                    break;
+                }
+            default: throw new System.Exception("ERROR: Enum had unrecognizable value.");
+        }
+    }
+}
