23b7a8f63240cd8d0eced03138d4773fc94c38a4
Jiri Korinek <kormaister@gmail.com>
2016-09-29 09:28:04 +0200

    Revert "Fixes"
    This reverts commit fdda165bd5ff011b556d0d277e8e5e8fed70d96f.

---
+++ Ymerdrengen2/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public enum Direction
+{
+    Left, Right, Up, Down
+}
+
+public abstract class Enemy : MonoBehaviour
+{
+
+    float despawnTime = 10;
+    float t = 0;
+
+    public float speed = 0;
+
+    public Direction direction;
+    int[][] attackPattern;
+
+    void Update()
+    {
+        behavior();
+        despawnCheck();
+    }
+
+    private void despawnCheck()
+    {
+        t += Time.deltaTime;
+        if (t >= despawnTime)
+            destroyThis();
+    }
+
+    public void destroyThis()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public abstract void behavior();
+    public abstract void init();
+    public abstract void init(int x, int y);
+    public abstract void init(int x, int y, Direction dir);
+
+}
