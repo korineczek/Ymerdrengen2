d381e5c10d4402452c82bde1181d842995b06915
Ciprian Florescu <ciprianliis@gmail.com>
2016-10-03 15:32:12 +0200

    AllIsHellLifeIsTorture

---
+++ Ymerdrengen2/Assets/Scripts/PossibleTilePosScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PossibleTilePosScript : MonoBehaviour
+{
+
+    GameObject grid;
+
+    // Use this for initialization
+    void Start()
+    {
+        grid = GameObject.Find("Grid");
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (grid.GetComponent<GridManager>().possiblePlacement)
+        {
+            StartCoroutine(Blink());
+        }
+        else
+        {
+            this.GetComponent<Renderer>().enabled = false;
+        }
+
+    }
+
+    IEnumerator Blink()
+    {
+        //float endTime = Time.time + waitTime;
+        while (grid.GetComponent<GridManager>().possiblePlacement)
+        {
+            this.GetComponent<Renderer>().enabled = false;
+            yield return new WaitForSeconds(0.8f);
+            this.GetComponent<Renderer>().enabled = true;
+            yield return new WaitForSeconds(0.8f);
+        }
+    }
+}
---
+++ Ymerdrengen2/Assets/WwiseTester.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WwiseTester : MonoBehaviour {
+
+    public string WwiseSoundIdentifier;
+    public string WwiseSwitchGroup;
+    public string WwiseSwitchName;
+    public AudioSource SupportSource;
+
+    // Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonDown(0) || Input.touchCount > 0) {
+            if (SupportSource != null) SupportSource.Play();
+
+            //AkSoundEngine.SetSwitch(WwiseSwitchGroup, WwiseSwitchName, gameObject);
+            AkSoundEngine.PostEvent(WwiseSoundIdentifier, gameObject);
+        }
+	}
+}
